dcde42eb7f1fc880aac2d292307976aae0aaa311
Hagar Nabil <47554625+hagar912@users.noreply.github.com>
2019-04-22 06:54:57 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,29 +1,31 @@
 library(dplyr)
 if(!file.exists("./getdata"){dir.create("./getdata")}
-fileurl <- ""
+fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 destfile <- "./getdata/dataset.zip"
 downoad.file(fileurl,destfile,method="curl")
 if(!file.exists("dataset.zip)){unzip(destfile),exdir="getdata")}
+
+## 1.Merges the training and the test sets to create one data set.
+
+# merge x , y to get new dataset
+# x=value ,y=activity
 
 # read test data
 x_test<-read.table("./UCI HAR Dataset/test/x_test.txt")
 y_test<-read.table("./UCI HAR Dataset/test/y_test.txt")
 subject_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+
 # read training data
 x_train <- read.table("./UCI HAR Dataset/train/x_train.txt")
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-
-
-## 1.Merges the training and the test sets to create one data set.
-# merge x , y to get new dataset
-# x=value ,y=activity
 x_dataset <- rbind(x_test,x_train)
 y_dataset <- rbind(y_test,y_train)
 subject_dataset <- rbind(subject_test, subject_train)
 
 
 ## 2.Extracts only the measurements on the mean and standard deviation for each measurement.
+
 #reding features table 
 features <- read.table("./UCI HAR Dataset/features.txt")
 #searching for mean , std 
@@ -48,7 +50,7 @@ names(subject_dataset) <- "subject"
 all_singledataset <- cbind(x_dataset, y_dataset, subject_dataset)
 colnames(all_singledataset) <- c("subject",features[,2],"activity")
 
-## 5.
+## 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 Data2<-aggregate(. ~subject + activity, all_singledataset, mean)
 Data2<-Data2[order(Data2$subject,Data2$activity),]
